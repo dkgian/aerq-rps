a7d14013660930e709cd91e05ae125c5a6c00ec6
dkgian <dkgian@gmx.de>
2022-02-18 11:45:13 +0100

    add some simple components

---
+++ src/components/Layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import Navigation from './Navigation'
+import Footer from './Footer'
+
+const Layout = () => {
+	return (
+		<>
+			<Header title="rps" />
+			<Navigation />
+			<Footer />
+		</>
+	)
+}
+
+export default Layout
