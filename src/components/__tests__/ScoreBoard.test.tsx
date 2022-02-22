5ee4ecf96a38477adb05bfd77381a8639c6073ca
dkgian <dkgian@gmx.de>
2022-02-22 19:22:15 +0100

    add rendering test score board

---
+++ src/components/__tests__/ScoreBoard.test.tsx
@@ -0,0 +1,22 @@
+import ScoreBoard from '../ScoreBoard'
+
+import {
+	render,
+	screen,
+} from '@testing-library/react'
+
+test('renders score board', () => {
+	render(
+		<ScoreBoard
+			p1={{ label: 'player 1', score: 1 }}
+			p2={{ label: 'player 2', score: 2 }}
+		/>
+	)
+	const p1Text = screen.getByText(/player 1/)
+	const p2Text = screen.getByText(/player 2/)
+	const score = screen.getByText(/1 : 2/)
+
+	expect(p1Text).toBeInTheDocument()
+	expect(p2Text).toBeInTheDocument()
+	expect(score).toBeInTheDocument()
+})
